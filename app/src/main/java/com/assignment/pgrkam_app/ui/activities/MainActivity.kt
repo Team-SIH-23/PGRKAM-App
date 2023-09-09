cdb2e6652276263d64a8eef8045a93423da2c5a1
Pratyush Singh <aries.pratyush@gmail.com>
2023-09-10 01:40:06 +0530

    chore: viewmodel setup to check output

--- app/src/main/java/com/assignment/pgrkam_app/ui/activities/MainActivity.kt
+++ app/src/main/java/com/assignment/pgrkam_app/ui/activities/MainActivity.kt
@@ -2,11 +2,44 @@ package com.assignment.pgrkam_app.ui.activities
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import androidx.lifecycle.Lifecycle
+import androidx.lifecycle.ViewModelProvider
+import androidx.lifecycle.lifecycleScope
+import androidx.lifecycle.repeatOnLifecycle
 import com.assignment.pgrkam_app.R
+import com.assignment.pgrkam_app.utils.GovtJobsUiState
+import com.assignment.pgrkam_app.viewmodels.GovtJobsViewModel
+import dagger.hilt.android.AndroidEntryPoint
+import kotlinx.coroutines.launch
 
+@AndroidEntryPoint
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var viewModel: GovtJobsViewModel
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        viewModel = ViewModelProvider(this)[GovtJobsViewModel::class.java]
+        viewModel.getGovtJobs()
+        lifecycleScope.launch {
+            repeatOnLifecycle(Lifecycle.State.STARTED) {
+                viewModel.govtJobsUiState.collect {
+                    when(it) {
+                        GovtJobsUiState.Error -> {
+
+                        }
+                        is GovtJobsUiState.GovtJobsList -> {
+                            Log.d("HERE",it.list.toString())
+                        }
+                        GovtJobsUiState.Loading -> {
+
+                        }
+                    }
+                }
+            }
+        }
     }
 }
