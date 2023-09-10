399b79119e965ebc317f8bfb2048e0d562320c53
Pratyush Singh <aries.pratyush@gmail.com>
2023-09-10 23:47:12 +0530

    feat: list of job sector & their profiles

--- app/src/main/java/com/assignment/pgrkam_app/ui/fragments/JobInterestFragment.kt
+++ app/src/main/java/com/assignment/pgrkam_app/ui/fragments/JobInterestFragment.kt
@@ -19,4 +19,60 @@ class JobInterestFragment : Fragment() {
         _binding = FragmentJobInterestBinding.inflate(inflater, container, false)
         return binding.root
     }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        val jobSector = arrayListOf(
+            "Agriculture",
+            "Apparel",
+            "Automotive",
+            "BFSI",
+            "Beauty & Wellness",
+            "Capital Goods & Manufacturing",
+            "Construction",
+            "Electronics & HW",
+            "GEM & Jewellery",
+            "HealthCare",
+            "IT",
+            "Leather",
+            "Mining",
+            "Organized Retail",
+            "Plumbing",
+            "Telecom",
+            "Tourism & Hospitality",
+            "Judiciary",
+            "Legal Activities",
+            "Optical Products",
+            "Power",
+            "Printing",
+            "Railways",
+            "Shipping"
+        )
+
+        val jobSectorProfile = arrayListOf(
+            "Analyst",
+            "Application Maintenance Engineer",
+            "Analyst-Research",
+            "Associate - CRM",
+            "Associate - Customer Care",
+            "Associate Analytics",
+            "Associate - SCM",
+            "Associate-Learning",
+            "Associate Network Engineer",
+            "Associate Operations Engineer",
+            "Associate-Transactional F & A",
+            "Associate-Clinical Data Management",
+            "Associate-Desktop Publishing",
+            "Associate-Editorial","Associate-Medical Transcription",
+            "Associate-HRO","Associate-Recruitment","Call Center Executive",
+            "Coding Clerk", "Collections Executive","Computer Programmer",
+            "Computer Security Specialist","CRM Domestic Voice", "Database Administrator",
+            "Junior Data Associate", "IP Executive", "Infrastructure Engineer"
+        )
+
+        binding.etJobSector.setSimpleItems(jobSector.toTypedArray())
+        binding.etJobSectorProfile.setSimpleItems(jobSectorProfile.toTypedArray())
+
+    }
 }
