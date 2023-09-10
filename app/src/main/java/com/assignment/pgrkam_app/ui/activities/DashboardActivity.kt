3cbffcb026d1ad946568691dd5fc64c2330404a1
Aditya Gupta <ashishgupta9936ad@gmail.com>
2023-09-10 15:42:48 +0530

    Implemented Navigation drawer on Dashboard Activity

--- app/src/main/java/com/assignment/pgrkam_app/ui/activities/DashboardActivity.kt
+++ app/src/main/java/com/assignment/pgrkam_app/ui/activities/DashboardActivity.kt
@@ -1,40 +1,40 @@
 package com.assignment.pgrkam_app.ui.activities
 
-import android.content.Context
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.util.Log
 import android.view.MenuItem
 import android.view.View
-import android.view.inputmethod.InputMethodManager
 import androidx.appcompat.app.ActionBarDrawerToggle
+import androidx.appcompat.app.AppCompatActivity
 import androidx.navigation.ui.AppBarConfiguration
 import com.assignment.pgrkam_app.R
 import com.assignment.pgrkam_app.databinding.ActivityDashboardBinding
 import com.assignment.pgrkam_app.databinding.ViewNavDrawerHeaderBinding
-import com.google.android.material.internal.ViewUtils.hideKeyboard
 import com.google.android.material.navigation.NavigationView
 import dagger.hilt.android.AndroidEntryPoint
 
 @AndroidEntryPoint
-class DashboardActivity : AppCompatActivity() , NavigationView.OnNavigationItemSelectedListener {
+class DashboardActivity : AppCompatActivity(), NavigationView.OnNavigationItemSelectedListener {
 
-    private lateinit var binding : ActivityDashboardBinding
+    private lateinit var binding: ActivityDashboardBinding
     private lateinit var navHeaderBinding: ViewNavDrawerHeaderBinding
+    private lateinit var appBarConfiguration: AppBarConfiguration
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityDashboardBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+        setSupportActionBar(binding.toolbarDashboard)
 
         setupNavigationBar()
 
-        setSupportActionBar(binding.toolbarDashboard)
-        setContentView(binding.root)
+        appBarConfiguration = AppBarConfiguration.Builder()
+            .setDrawerLayout(binding.drawer)
+            .build()
     }
 
     override fun onNavigationItemSelected(item: MenuItem): Boolean {
-        when(item.itemId){
-            R.id.local_services-> {
+        when (item.itemId) {
+            R.id.local_services -> {
 
             }
 
@@ -51,10 +51,6 @@ class DashboardActivity : AppCompatActivity() , NavigationView.OnNavigationItemS
             this,
             binding.drawer, binding.toolbarDashboard, R.string.app_name, R.string.close_drawer
         ) {
-
-            override fun onDrawerOpened(drawerView: View) {
-                super.onDrawerOpened(drawerView)
-            }
 
             override fun onDrawerSlide(drawerView: View, slideOffset: Float) {
                 if (slideOffset != 0f) super.onDrawerSlide(drawerView, slideOffset)
