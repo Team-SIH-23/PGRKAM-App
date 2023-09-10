7bc8e889382c1b654823cf43be373f5be0f269bb
Pratyush Singh <aries.pratyush@gmail.com>
2023-09-11 01:03:11 +0530

    feat: view pager in jobs fragment

--- app/src/main/java/com/assignment/pgrkam_app/ui/fragments/JobFragment.kt
+++ app/src/main/java/com/assignment/pgrkam_app/ui/fragments/JobFragment.kt
@@ -6,10 +6,13 @@ import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
 import com.assignment.pgrkam_app.databinding.FragmentJobBinding
+import com.assignment.pgrkam_app.ui.adapters.ViewPagerAdapter
+import com.google.android.material.tabs.TabLayoutMediator
 
 class JobFragment : Fragment() {
 
     private lateinit var binding: FragmentJobBinding
+    var tabTitle = arrayOf("Most Suitable Jobs", "Applied Jobs", "Scheduled Job Interview")
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
@@ -18,5 +21,17 @@ class JobFragment : Fragment() {
         binding = FragmentJobBinding.inflate(inflater, container, false)
 
         return binding.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        val pager = binding.viewPager
+        pager.isSaveEnabled = false
+        val tl = binding.tabs
+        pager.adapter = ViewPagerAdapter(childFragmentManager, lifecycle)
+
+        TabLayoutMediator(tl, pager) { tab, position ->
+            tab.text = tabTitle[position]
+        }.attach()
     }
 }
