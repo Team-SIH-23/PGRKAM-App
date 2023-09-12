260ce1cc1aac90680cbbf36fdda675d0ec0dfb07
Pratyush Singh <aries.pratyush@gmail.com>
2023-09-12 22:48:33 +0530

    feat: post hog setup

--- app/src/main/java/com/assignment/pgrkam_app/PgrkamApplication.kt
+++ app/src/main/java/com/assignment/pgrkam_app/PgrkamApplication.kt
@@ -1,8 +1,21 @@
 package com.assignment.pgrkam_app
 
 import android.app.Application
+import com.posthog.android.PostHog
 import dagger.hilt.android.HiltAndroidApp
 
+
 @HiltAndroidApp
-class PgrkamApplication: Application() {
+class PgrkamApplication : Application() {
+
+    override fun onCreate() {
+        super.onCreate()
+        val posthog =
+            PostHog.Builder(this, "phx_mNTyr58SbJPVvEl0Yk79lM3zMFKEA0cNoHDYrWzoqpw")
+                .captureApplicationLifecycleEvents()
+                .recordScreenViews()
+                .build()
+        PostHog.setSingletonInstance(posthog)
+    }
+
 }
